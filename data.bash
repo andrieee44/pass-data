196e5600661144b02e3704e328fb66d3d9bfd4b3
nix-on-droid <andrieee44@gmail.com>
2025-02-08 12:57:10 +0800

    set -e not working for some reason

--- data.bash
+++ data.bash
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
+set -x
 
 set -eo pipefail
 
 [ "$#" -lt 2 ] && {
-	echo "usage: ${PROGRAM} data <DIRECTORY> <PROGRAM> ARGS ..." >&2
+	echo "usage: ${PROGRAM} data <DIRECTORY> <PROGRAM> ARGS ..." >&2 || exit 1
 	exit 1
 }
 
@@ -13,32 +14,35 @@ passTar="${PREFIX}/${path}.gpg"
 passTarDir="${passTar%"/${path}.gpg"}"
 passName="${passTar%.gpg}"
 passName="${passName##*/}"
-mkdir -p -- "$passTarDir"
-set_gpg_recipients "$passTarDir"
-set_git "$passTar"
-tmpdir
+mkdir -p -- "$passTarDir" || exit 1
+set_gpg_recipients "$passTarDir" || exit 1
+set_git "$passTar" || exit 1
+tmpdir || exit 1
 tmpPath="${SECURE_TMPDIR}/${passName}"
 tmpTar="${tmpPath}.tar.gz"
 tmpPath2="${SECURE_TMPDIR}/old.${passName}"
 tmpTar2="${tmpPath2}.tar"
-mkdir -p -- "$tmpPath" "$tmpPath2"
+mkdir -p -- "$tmpPath" "$tmpPath2" || exit 1
 
 [ -f "$passTar" ] && {
-	$GPG -d -o "$tmpTar" "${GPG_OPTS[@]}" "$passTar"
-	gzip -d "$tmpTar"
-	tar -xf "${tmpTar%.gz}" -C "$tmpPath"
-	cp -rf "$tmpPath/." "$tmpPath2"
+	$GPG -d -o "$tmpTar" "${GPG_OPTS[@]}" "$passTar" || exit 1
+	gzip -d -- "$tmpTar" || exit 1
+	tar -xf "${tmpTar%.gz}" -C "$tmpPath" || exit 1
+	cp -rf -- "$tmpPath/." "$tmpPath2" || exit 1
 }
 
 prog="$2"
 shift 2
 PASS_DATA="$tmpPath" eval "${prog} ${*}"
 
-[ -f "$passTar" ] && diff -r "$tmpPath" "$tmpPath2" >/dev/null 2>&1  && return
+[ -f "$passTar" ] && {
+	which diff || exit 1
+	diff -r -- "$tmpPath" "$tmpPath2" && exit 0
+}
 
-tar -cf "$tmpTar2" -C "$tmpPath" .
-gzip "$tmpTar2"
+tar -cf "$tmpTar2" -C "$tmpPath" -- . || exit 1
+gzip -- "$tmpTar2" || exit 1
 
-$GPG -e "${GPG_RECIPIENT_ARGS[@]}" -o "$passTar" "${GPG_OPTS[@]}" "${tmpTar2}.gz"
+$GPG -e "${GPG_RECIPIENT_ARGS[@]}" -o "$passTar" "${GPG_OPTS[@]}" "${tmpTar2}.gz" || exit 1
 
 git_add_file "$passTar" "Update data in ${path}."
